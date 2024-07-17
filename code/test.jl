549ff40f3091234b9f947d1907a741c7c25856f2
Div <dbhagia17@gmail.com>
2024-07-17 12:46:12 -0700

    minor edits

--- code/test.jl
+++ code/test.jl
@@ -9,7 +9,7 @@ include("utils_est.jl")
 Random.seed!(1117)
 
 # Parameters
-n = 5000000
+n = 1000000
 p = 0.5
 T_bar = 6
 ϕ = [1, 2]
@@ -31,7 +31,7 @@ g = sim_data(n, T_bar, p, ϕ, ν_pars, opt)
 println("Model vs data moments: ", sum((g - g_model).^2))
 
 # Estimate parameters when true moments given
-Θ_hat = gmm(g_model, nrm)
+@time Θ_hat = gmm(g, nrm)
 
 # Print error in estimation
 error = sum((Θ_hat - Θ).^2)
